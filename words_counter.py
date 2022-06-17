0f5b4d507eec2a0f8ec2c8477e607a9a55664390
Egor <egor-popovich.ru@mail.ru>
2022-06-17 18:49:57 +0300

    word sorter ready

--- words_counter.py
+++ words_counter.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 
-example_string = 'привет пока как дела машина привет хорошо пока солнце привет хорошо пока заяй привет'
+example_string = 'привет пока как дела машина привет хорошо пока солнце привет хорошо пока заяц привет yes yes yes'
+
+
+print(f'{"Optimized variant":-^45}')
 
 
 def count_words_with_default_dict(string, n=1):
@@ -8,13 +11,16 @@ def count_words_with_default_dict(string, n=1):
     string = string.split(' ')
     for word in string:
         words_dict[word] += 1
-    return words_dict
+
+    words_dict = {k: v for k, v in sorted(words_dict.items(), key=lambda item: item[1])}
+    for _ in range(n):
+        key, value = words_dict.popitem()
+        print(f'{key}: {value}')
 
 
-result = count_words_with_default_dict(example_string)
-for key, value in result.items():
-    print(f'{key}: {value}')
-print(f'{"Second variant":-^45}')
+count_words_with_default_dict(example_string, n=3)
+print(f'{"Simple variant":-^45}')
+
 
 def count_words(string, n=1):
     words_dict = {}
@@ -27,9 +33,15 @@ def count_words(string, n=1):
         else:
             raise ValueError
 
-    return words_dict
+    for _ in range(n):
+        maximum = 0
+        max_key = None
+        for key, value in words_dict.items():
+            if value >= maximum:
+                maximum = value
+                max_key = key
+
+        print(f'{max_key}: {words_dict.pop(max_key)}')
 
 
-res = count_words(example_string)
-for key, value in res.items():
-    print(f'{key}: {value}')
+count_words(example_string, n=3)
